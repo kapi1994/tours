b5419972405acc0be1e3523c9b01817ee57cfbbb
Nemanja Jovicic <nemanjajovicic13@gmail.com>
2024-06-12 02:40:02 +0200

    Admin home page

--- includes/pages/admin/home.php
+++ includes/pages/admin/home.php
@@ -1,1 +1,39 @@
-hoem
+<main>
+    <section class="container">
+        <div class="row mt-5">
+            <div class="col-lg-8 mb-2 mb-lg-0">
+               <div class="table-responsive-sm table-responsive-md">
+                <table class="table text-center align-middle">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Name</th>
+                            <th scope="col">Date</th>
+                            <th scope="col">Number of reservations</th>
+                        </tr>
+                    </thead>
+                    <tbody id="reservations">
+                        
+                    </tbody>
+                </table>
+               </div>
+            </div>
+            <div class="col-lg-4">
+                <div class="d-grid gap-1">
+                    <div class="col mb-2">
+                       <div class="card text-center py-3">
+                            <h5 class="mb-2">Broj registrovanih korisnika</h5>
+                            <h6 id="numberOfUsers" class="display-4 fw-bold"></h6>
+                       </div>
+                    </div>
+                    <div class="col">
+                    <div class="card text-center py-3">
+                            <h5 class="mb-2">Broj aktivnih destinacija</h5>
+                            <h6 id="numberOfTours" class="display-4 fw-bold"></h6>
+                       </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</main>
