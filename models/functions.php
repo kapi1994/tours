e3c06331b8814711cbd8a1cdc2bc3509ef9c38c0
Nemanja Jovicic <nemanjajovicic13@gmail.com>
2024-06-12 02:09:24 +0200

    Added new functions

--- models/functions.php
+++ models/functions.php
@@ -2,13 +2,14 @@
 // global 
 
 define("ADMIN_OFFSET",5);
+define("USER_OFFSET",12);
 
 function getOne($query){
     global $connection;
     return $connection->query($query)->fetch();
 }
 
-function getAll($query){
+function getAll($query){    
     global $connection;
     return $connection->query($query)->fetchAll();
 }
@@ -79,8 +80,13 @@ return getAll("select id, name from categories");
 
 }
 
-function getAllTours(){
+function getAllTours($limit = 0){
     $queryTours = "select * from tours";
+    
+    $offset = (int) ADMIN_OFFSET;
+    $limit = (int)$limit * $offset;
+    $queryLimit = " limit $limit, $offset";
+    $queryTours.= $queryLimit;
     $tours = getAll($queryTours);
     foreach($tours as $tour){
         $queryCategory = "select name from categories
@@ -89,6 +95,12 @@ function getAllTours(){
         $tour->categories = getAll($queryCategory);
     }
     return $tours;
+}
+
+function getAllToursPagination(){
+    $query = "select count(*) as numberOfElements from tours";
+    $data = getOne($query);
+    return ceil($data->numberOfElements / ADMIN_OFFSET);
 }
 
 function getOneTour($id){
@@ -182,7 +194,7 @@ function getOneDate($id){
     return getOne("select id, date from tour_dates where id = '$id'");
 }
 function getOneDateFullRow($id){
-    return getOne("select * from tour_dates where tour_id = '$id'");
+    return getOne("select * from tour_dates where id = '$id'");
 }
 function checkTourDate($date, $tour_id){
     $query = "select id, date, tour_id from tour_dates where tour_id= '$tour_id' and date= '$date'" ;
@@ -200,11 +212,11 @@ function updateTourDate($date, $id){
     global $connection;
     $date_update= date("Y-m-d H:i:s");
     $queryUpdate = "update tour_dates set  date = ?, updated_at  = ? where id =?";
+
     $update = $connection->prepare($queryUpdate);
     $update->execute([$date, $date_update, $id]);
 }
 
-// survey options
 
 // questions
 function getAllQuestions(){
@@ -323,7 +335,7 @@ function checkActiveSurvey(){
 function updateStatusSurvey(){
     global $connection;
     $is_active = 0;
-    $query = "update * from survey where is_active  = ?";
+    $query = "update survey set is_active = ?";
     $udpateQuery = $connection->prepare($query);
     $udpateQuery->execute([$is_active]);
 }
@@ -333,14 +345,27 @@ function insertSurvey($date, $question, $answers){
     if(checkActiveSurvey()){
         updateStatusSurvey();
     }else{
-       
         $query = "insert into survey (expire_date, question_id) values(?,?)";
         $insert = $connection->prepare($query);
         $insert->execute([$date, $question]);
         $last_id = $connection->lastInsertId();
-
-        insertIntoPivotTable('survey_answers', 'survey_id', 'answer_id',$answers, $last_id);
+        insertIntoSurveyAnswer($last_id, $answers);
     }
+}
+
+function insertIntoSurveyAnswer($survey_id, $answers){
+    global $connection;
+    $query = "insert into survey_answers (survey_id, answer_id)";
+    $surveyParam = [];
+    $surveyValues = [];
+    foreach($answers as $answer){
+        $surveyParam[]= "(?,?)";
+        $surveyValues[]=  (int)$survey_id;    
+        $surveyValues[] = (int)$answer;
+    }
+    $query.= " values".implode(',',$surveyParam);
+    $insert = $connection->prepare($query);
+    $insert->execute($surveyValues);
 }
 
 function getAllSurveys(){
@@ -364,5 +389,129 @@ function updateSurvey($survey_id, $date, $question, $answers){
     $update = $connection->prepare($queryUpdate);
     $update->execute([$date, $question, $update_date, $survey_id]);
     deleteData('survey_answers', 'survey_id', $survey_id);
-    // insertIntoPivotTable('survey_answers', 'survey_id', 'answer_id', $answers, $survey_id);
+    insertIntoSurveyAnswer($survey_id, $answers);
+}
+
+function getSurveyOneFullRow($id){
+    return getOne("select  s.*, q.name as questionName 
+    from survey s join questions q on s.question_id = q.id where s.id = '$id'");
+}
+
+function checkIfIsAnyActive(){
+    $query = "select count(*) as numberOfSurveys from survey where is_active = 1";
+    return getOne($query);
+}
+
+function changeStatusSurvey($id, $status){
+    global $connection;
+    $status = $status == 1 ? 0 : 1;
+    $query = "update survey set is_active =? where id =?";
+    $update = $connection->prepare($query);
+    $update->execute([$status, $id  ]);
+}
+
+   
+// others 
+function numberOfRegistretedUsers(){
+    $query = "select count(*) as numberOfUsers from users where role_id = 2";
+    return getOne($query);
+}
+function numberOfTours(){
+    $query  = "select count(*) as numberOfTours from tours where is_deleted = 0";
+    return getOne($query);
+}
+
+//
+function getAvailableTours($limit =  0,$search = '', $categories= ''){
+    // SELECT * from tours t join category_tour ct on t.id = ct.tour_id where ct.category_id in (1);
+    $queryWhere = " where is_deleted = 0";
+    $baseQuery = "select  t.id, t.name, t.image_path from tours t ";
+    if($search != ""){
+        $compareString = trim("%$search%");
+        $queryWhere.=" and name like '$compareString'";
+    }
+    if($categories != ""){
+        $baseQuery.= "join category_tour ct on t.id = ct.tour_id";
+        $queryWhere .= " and ct.category_id in (".$categories.")";
+    }
+    $offset = (int)USER_OFFSET;
+    $limit = (int)$limit * $offset;
+    $queryLimit = " limit $limit, $offset";
+    $query = $baseQuery.$queryWhere.$queryLimit;
+    // return $query;
+    return getAll($query);
+}
+
+function getAvaliableToursPagination($search ="", $categories= ""){
+    $baseQuery = "select count(*) as numberOfElements from tours t";
+    $queryWhere = "  where is_deleted = 0";
+    if($search != ""){
+        $compareString = trim("%$search%");
+        $queryWhere .= " and t.name like '$compareString'";
+    }
+    if($categories != ""){
+        $baseQuery.= " join category_tour ct on t.id = ct.tour_id";
+        $queryWhere .= " and ct.category_id in (".$categories.")";
+    }
+    $query = $baseQuery.$queryWhere;
+    $data = getOne($query);
+    return ceil($data->numberOfElements / USER_OFFSET);
+}
+
+function getTheMostVisitedTours(){
+    $query = "select COUNT(*) as mostVisitedTours, t.name, td.date from reservations  utd join tour_dates td on utd.tour_date_id = td.id
+    join tours t on td.tour_id = t.id GROUP BY tour_date_id ORDER BY mostVisitedTours DESC";
+    return getAll($query);
+}
+
+// user
+function getAvailableTourDate($tour_id){
+    $query = "select id, date from tour_dates where is_deleted = 0 and tour_id = '$tour_id' ORDER BY date ASC";
+   return getAll($query);
+}
+
+function getTourData($tour_id){
+    $query = "select * from tours where id= '$tour_id'";
+    return getOne($query);
+}
+
+//  nav fucntions
+function getUserMenu(){
+    return getAll('select * from user_menu');
+}
+function getAdminMenu(){
+    return getAll("select * from admin_menu");
+}
+
+function storeNewReservation( $tour_date, $user_id){
+    global $connection;
+    $query = "insert into reservations (tour_date_id, user_id) values(?,?)";
+    $insert = $connection->prepare($query);
+    $insert->execute([$tour_date, $user_id]);
+}
+
+function getAvailableQuestion(){
+    $query = "select s.id, q.name from survey s join questions q on s.question_id = q.id where q.is_deleted = 0";
+    return getOne($query);
+}
+
+function getSurveyOptions($survey_id){
+    $query ="select sa.answer_id, a.name from survey_answers sa join answers a on sa.answer_id = a.id  where sa.survey_id = '$survey_id'";
+    return getAll($query);
+}
+
+function checkIfUserVote($user_id, $survey_id){
+    $query ="select * from survey_vote where user_id = '$user_id' and id_survey='$survey_id'";
+    return getOne($query);
+}
+
+function insertVote($survey_id, $option_id, $user_id){
+    global $connection;
+    $queryInsert = $connection->prepare("insert into survey_vote (id_survey, id_answer, user_id) values(?,?,?) ");
+    $queryInsert->execute([$survey_id,$option_id, $user_id]);   
+}
+
+function getMainTours(){
+    $baseQuery = "select  t.id, t.name, t.image_path from tours t limit 4 ";
+    return getAll($baseQuery);
 }
