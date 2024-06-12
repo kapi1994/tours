a4138fa36f60a367ae122fc13a21e8f60cb70c62
Nemanja Jovicic <nemanjajovicic13@gmail.com>
2024-06-12 02:34:12 +0200

    Author page

---
+++ includes/pages/user/author.php
@@ -0,0 +1,13 @@
+<main>
+    <section class="container">
+        <div class="row mt-5">
+            <div class="col-lg-4 mx-auto">
+                <img src="assets/img/127789955.jpeg" alt="nemanja jovicic" class="img-fluid"/>
+                <p class="text-center my-3">
+                    Moje ime je <span class="fw-bold">Nemanja Jovici</span> dolazim
+                    iz Uba. Moji hobiji su planinarenje i igranje video igara.
+                </p>
+            </div>
+        </div>
+    </section>
+</main>
